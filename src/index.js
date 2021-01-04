415280b5ff5f3c6ad7eb08c9c2de4412bf93aee2
zongxiong.chen <czxczf@gmail.com>
2021-01-04 15:52:50 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,53 @@
+const sdk = require('matrix-js-sdk');
+const bodyParser = require('body-parser');
+const express = require('express');
+const config = require('./config');
+console.log(config);
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.post('/', function (req, res) {
+  res.send('Hello World');
+})
+
+app.post('/notify', function (req, res) {
+  if (req.headers.authorization != config.basicAuthorization) {
+    console.log('Error Authorization');
+    return res.send('Error');
+  }
+
+  const { roomId, accessToken, userId, homeServerUrl } = config;
+  const client = sdk.createClient({
+    baseUrl: homeServerUrl,
+    accessToken: accessToken,
+    userId: userId,
+  });
+  console.log(req.body);
+
+  const body = `<h2>${req.body.title}</h2><a href=\"${req.body.ruleUrl}\">${req.body.ruleName}</a><p>${req.body.message}</p>`;
+
+  const content = {
+    body: "LitentryBot: \n```json\nAlter\n```",
+    formatted_body: body,
+    format: 'org.matrix.custom.html',
+    msgtype: 'm.text',
+  };
+
+  client.sendEvent(roomId, 'm.room.message', content, '', (err, res) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(res);
+    }
+  });
+
+  res.send('Ok');
+})
+
+const port = 3000;
+const server = app.listen(port, function () {
+  const host = server.address().address;
+  const port = server.address().port;
+  console.log(`RiotBot listening at http://${host}:${port}`);
+})
